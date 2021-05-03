8ba0e48049947ff267a24806dd5c145e4ac8331b
Seung-yeonLee <sie9412@gmail.com>
2021-05-03 23:41:04 +0900

    learn reusable Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import Leo from './Leo';
+
+function Dogs({name}) { 
+  return <h3>I love {name}</h3>
+}
 
 function App() {
   return (
     <div className="App">
       <h1>First App</h1>
-      <Leo />
+      <Dogs 
+      name="Leo"
+      hungry={true} 
+      info = {["male", "8 months old", 'retriever']} />
+      <Dogs 
+      name="May"
+      hungry={false} 
+      info = {["female", "2 years old", 'German shepard']} />
+      <Dogs 
+      name="Jenny"
+      hungry={true} 
+      info = {["female", "10 years old", 'retriever']} />
     </div>
   );
 }
--- src/Leo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Leo() {
-    return <h3>I love Leo</h3>;
-}
-
-export default Leo;

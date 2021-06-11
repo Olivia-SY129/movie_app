73a228eebd6874c0febf2ebbb47010a84555d8c3
Seung-yeon Lee <sie9412@gmail.com>
2021-06-11 23:27:34 +0900

    detail page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function About() {
-    return (
-        <span>About this page</span>
-    )
+  return (
+    <div className="about">
+      <h1 className="about__app__title">Movie App 2021</h1>
+      <div className="about__author__title">Design with Code</div>
+      <div className="about__author__name">STR21</div>
+      <div className="about__author__contact">Contact: sie94@naver.com</div>
+    </div>
+  );
 }
 
 export default About;
